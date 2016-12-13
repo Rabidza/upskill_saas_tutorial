4539092d19a057c74e0f8c734dda7bd9f33f0c5e
Neill Henning <neillhenning@gmail.com>
2016-12-13 22:45:42 +0000

    Updated email address

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'neillhenning@gmail.com'
+  default to: 'dev.hbit@gmail.com'
   def contact_email(name, email, body)
     @name = name
     @email = email
